1f5c63fb7d1c9ce75027f5ee88be013af1b44371
Paweł Klockiewicz <eklocek@gmail.com>
2021-06-30 19:54:09 +0200

    fix: Correction of the main regex

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   maxFields: 5,
-  postRegex: 'https*:\/\/www.wykop.pl\/wpis\/([0-9]+)\/(.*)',
+  postRegex: 'https*:\/\/www.wykop.pl\/wpis\/([0-9]+)(.*)',
   commentRegex: '#comment\-([0-9]+)',
   userRegex: 'ludzie\/([^\/]+)',
   endpoint: 'https://www.wykop.pl/ajax2/wpis',
